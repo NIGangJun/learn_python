188df511c823316565519d6190832c7ef4075583
JunFang <ngjqaz@126.com>
2022-12-06 09:48:52 +0800

    学习：python操作pgsql请求。未完

---
+++ pg_learn/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Author     : NIGangJun
+@Date       : 2022/12/5 16:42
+@File       : __init__.py.py
+@Desc       : None
+"""
---
+++ pg_learn/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Author     : NIGangJun
+@Date       : 2022/12/5 16:42
+@File       : main.py
+@Desc       : 操作数据库
+"""
