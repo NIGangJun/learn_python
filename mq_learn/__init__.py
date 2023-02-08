393be14dab75c728fe8acdbeec32865b356178bb
NIGangJun <ngjqaz@126.com>
2023-02-08 18:02:01 +0800

    mq的使用考虑使用配置文件直接读取

---
+++ mq_learn/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author     : NIGangJun
+@Date       : 2023/1/18 11:40
+@File       : __init__.py.py
+@Desc       : 
+"""
+from . import msmq_learn
+from . import rabbitmq_learn
